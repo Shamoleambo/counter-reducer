568adaf25275eb9ffc52256d4508117cd15ccc9d
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-16 09:51:19 -0300

    make the reducer the star of the show

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
+import { useReducer } from 'react'
+
+const initialState = { count: 0 }
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + 1 }
+    case 'decrement':
+      return { count: state.count - 1 }
+    case 'reset':
+      return { count: 0 }
+    default:
+      throw new Error()
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
   return (
-    <>
-    </>
+    <div className='app'>
+      <h1>Counter with reducer</h1>
+      <p>Count: {state.count}</p>
+      <button onClick={() => dispatch({ type: 'increment' })}>Increment</button>
+      <button onClick={() => dispatch({ type: 'decrement' })}>Decrement</button>
+      <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
+    </div>
   )
 }
 
